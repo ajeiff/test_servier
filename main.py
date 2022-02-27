6b99d11fd547e75e1014c888668a27398de3880c
ajeiff <ajeiff@gmail.com>
2022-02-27 23:55:11 +0100

    part 4 exercise

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 from cleaning_csv.common_utils import create_formated_date
 from cleaning_csv.clinical_trials import clean_trials
-
+import operator
 import numpy as np
 
 class NpEncoder(json.JSONEncoder):
@@ -67,6 +67,27 @@ def create_json_dict(df_pubmed, df_trials, df_drugs):
         json_dict[small_single_string(name_drug)[0]] = publications(df_pubmed=df_pubmed_clean, df_trials=df_trials_clean, name_drug=name_drug)
     return json_dict
 
+def top_mentionning_journal(data):
+#find the name of the journal that has the highest number of different drugs mentioned
+    dict_journal = dict()
+    # explore each item of the json file by key, so to say by drug
+    for drug in list(data.keys()):
+        list_journal_drug = list()
+        for i in range(len(data[drug])):
+            curr_journal = str(data[drug][i]['journal'])
+            if curr_journal not in list_journal_drug:
+                # append a temporary list per key to have the list of all the journals that mention the current drug
+                list_journal_drug.append(curr_journal)
+        for elt in list_journal_drug:
+            # increment the correspond value of the final dict each time a new drug is mentionned in the journal
+            if elt in list(dict_journal.keys()):
+                dict_journal[elt] += 1
+            else:
+                dict_journal[elt] = 1
+    #find the journal, so to say the key, that has the highest value
+    result = max(dict_journal.items(), key=operator.itemgetter(1))[0]
+    return result
+
 
 if __name__ == '__main__':
     df_trials = pd.read_csv('cleaning_csv/csv/clinical_trials.csv')
@@ -80,3 +101,14 @@ if __name__ == '__main__':
 
     with open("output.json", "w") as write_file:
         json.dump(json_dict, write_file)
+
+
+
+    ## part 4 of the exercise: get the journal with the highest number of different drugs mentionned
+
+    ## loading the generated json file
+    # with open('output.json', 'r') as f:
+    #     data = json.load(f)
+
+    ## call the function defined as above
+    #top_mentionning_journal(data)
